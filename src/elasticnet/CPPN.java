ad9a3da332ce1fdd019ae680c22c45520eb495ac
Nick Williams <nick@striderbikes.com>
2018-10-26 15:59:04 -0600

    i want all the dimensions sir

---
+++ src/elasticnet/CPPN.java
@@ -0,0 +1,10 @@
+package elasticnet;
+
+import java.util.ArrayList;
+
+public class CPPN {
+	ArrayList<CPPN_Node> nodes = new ArrayList<CPPN_Node>();
+	public CPPN(ArrayList<CPPN_Node> node_list) {
+		this.nodes = node_list;
+	}
+}
