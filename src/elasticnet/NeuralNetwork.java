458e2e5b066a84a8b03075d8416315a7e9201c6d
nick williams <nick5664@outlook.com>
2019-03-02 07:47:23 -0700

    just have to make activation methods and handle recursion, other net implementations

--- src/elasticnet/NeuralNetwork.java
+++ src/elasticnet/NeuralNetwork.java
@@ -3,16 +3,25 @@ package elasticnet;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class NueralNetwork implements INeuralNet {
+public class NeuralNetwork implements INeuralNet {
 
 	public int current_layer = 0;
 	public int num_activations = 0;
-	HashMap<Integer, ArrayList<INode>> nodes;
+	ArrayList<INode> in_nodes;
+	ArrayList<INode> out_nodes;
+	ArrayList<INode> hidden_nodes;
+	
+	public NeuralNetwork(ArrayList<INode> i_nodes, ArrayList<INode> o_nodes, ArrayList<INode> h_nodes)
+	{
+		this.in_nodes = i_nodes;
+		this.out_nodes = o_nodes;
+		this.hidden_nodes = h_nodes;
+	}
 	
 	@Override
 	public void Activate() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
