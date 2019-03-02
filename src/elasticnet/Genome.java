e8c235969383408731120f6fca5280883d573dfe
nick williams <nick5664@outlook.com>
2019-03-02 07:33:01 -0700

    just need to make nets and handle speciation

--- src/elasticnet/Genome.java
+++ src/elasticnet/Genome.java
@@ -2,7 +2,7 @@ package elasticnet;
 import java.util.HashMap;
 import java.util.List;
 
-public class Genome implements INeuralNet {
+public class Genome {
 
 	public int id;
 	int gen_born;
@@ -75,23 +75,5 @@ public class Genome implements INeuralNet {
 	public int get_pop_id()
 	{
 		return this.population_hash;
-	}
-
-	@Override
-	public void Activate() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void Reset() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void get_output() {
-		// TODO Auto-generated method stub
-		
 	}
 }
