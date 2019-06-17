f1a802693e98416319ea33b41da5051ac324b85b
nick williams <nick5664@outlook.com>
2019-06-17 05:59:13 -0600

    xbtusd saved

--- src/XchangeService/TestMex.java
+++ src/XchangeService/TestMex.java
@@ -1,18 +1,25 @@
 package XchangeService;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import com.google.gson.Gson;
 
 public class TestMex {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		BitmexHistWriter test_writer = new BitmexHistWriter();
 		
 		//System.out.print(test_writer.all_pairs);
 		
 		String data = test_writer.get_from_mex("");
 		
-		HistDataBar[] container = new Gson().fromJson(data, HistDataBar[].class);
 		
-		System.out.println(container[0].close);
+		try (PrintWriter out = new PrintWriter("xbtusd.json")) {
+		    out.println(data);
+		}
+		//HistDataBar[] container = new Gson().fromJson(data, HistDataBar[].class);
+		
+		//System.out.println(container[0].close);
 	}
 }
