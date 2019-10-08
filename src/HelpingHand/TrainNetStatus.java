3d7d3a0ec895edc17e04a1497bfb5d9a5d381885
nick williams <nick5664@outlook.com>
2019-10-08 05:48:28 -0600

    working on protocol code

---
+++ src/HelpingHand/TrainNetStatus.java
@@ -0,0 +1,14 @@
+package HelpingHand;
+
+public class TrainNetStatus {
+	
+	public String status = "";
+	
+	public String ts_hash = "";
+	
+	public TrainNetStatus(String stat, String ts)
+	{
+		this.status = stat;
+		this.ts_hash = ts;
+	}
+}
