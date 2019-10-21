133aceb2117af803a00d8e6fafef1b1905a5ddb7
nick williams <nick5664@outlook.com>
2019-10-21 10:52:04 -0600

    mkay

--- src/elasticnet/Population.java
+++ src/elasticnet/Population.java
@@ -10,6 +10,7 @@ import com.google.gson.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * 
@@ -117,7 +118,7 @@ public class Population {
 			else
 			{
 				//TODO check and make sure genomes min and max get set
-				if(one_id >= two.gene_id_min && one_id <= two.gene_id_max)
+				if(one_id >= Collections.min(two_conns) && one_id <= Collections.max(two_conns))
 				{
 					d += 1.0;
 				}
