ae831576d218a7547cee3c72c06a6c643567150a
nick williams <nick5664@outlook.com>
2019-12-14 22:49:03 -0700

    has to be done

--- src/elasticnet/BaseGenome.java
+++ src/elasticnet/BaseGenome.java
@@ -29,6 +29,7 @@ public class BaseGenome {
 	public boolean is_recursive = false;
 	public String peer_eval_id = "";
 	public String peer_validation_id = "";
+	public InnovationService inno_service = new InnovationService();
 	
 	//TODO set max and min conn in all mutation methods
 	//TODO and upon construction
@@ -43,7 +44,60 @@ public class BaseGenome {
 		fitness = test_fit;
 	}
 	
+	public int mutate_genome(int new_id, 
+			NeatConfig config,
+			HashMap<Integer, HashMap<Integer,NodeGene>> pop_nodes, 
+			HashMap<Integer, HashMap<Integer,ConnectionGene>> pop_conns
+			)
+	{
+		Random rand = new Random();
+		
+		String default_activation = config.defaultActivation;
+		
+		this.mutate_weights(config.mutate_weight_factor, config.weight_mutate_rate, config.weight_min, config.weight_max, pop_nodes,  pop_conns);
+		
+		// sum of all mutation probabilities
+		Double prob_sum = config.add_conn_prob + config.delete_conn_prob + config.add_node_prob + config.delete_node_prob;
+		
+		// if sum is less than 1.0 set sum to be 1.0
+		
+		while(new_id <= Collections.max(pop_conns.keySet()) || new_id <= Collections.max(pop_nodes.keySet()))
+		{
+			new_id++;
+		}
 	
+		if (prob_sum < 1.0)
+		{
+			prob_sum = 1.0;
+		}
+		if (rand.nextFloat() < (config.delete_node_prob/prob_sum))
+		{
+			System.out.println("deleting node here");
+			mutate_delete_node(pop_conns, pop_nodes);
+		}
+		if (rand.nextFloat() < (config.delete_conn_prob/prob_sum))
+		{
+			System.out.println("deleting conn here");
+			mutate_delete_conn(pop_conns, pop_nodes);
+		}
+		if (rand.nextFloat() < (config.add_conn_prob/prob_sum))
+		{
+			System.out.println("adding conn here");
+			new_id = mutate_add_conn(new_id, pop_conns, pop_nodes);
+		}
+		if (rand.nextFloat() < (config.add_node_prob/prob_sum))
+		{
+			//this is where we are loosing conn pointers from node genes
+			System.out.println("adding node here");
+			new_id = mutate_add_node(new_id, config.defaultActivation, pop_nodes, pop_conns);
+		}
+		return new_id;
+	}
+	
+	public void mutate_weights(double rate, double factor, double min, double max)
+	{
+		return;
+	}
 	private int get_random_in_range(int range_len)
 	{
 		Random dice = new Random();
---
+++ src/elasticnet/InnovationService.java
@@ -0,0 +1,28 @@
+package elasticnet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class InnovationService {
+	public ArrayList<Long> node_ids;
+	
+	public HashMap<Long, long[]> conn_coo;
+	
+	public InnovationService()
+	{
+		this.node_ids = new ArrayList<Long>();
+		this.conn_coo = new HashMap<Long, long[]>();
+	}
+	
+	public Long get_next_node_id()
+	{
+		long next = Collections.max(node_ids);
+		return next;
+	}
+	
+	public long get_next_conn_id()
+	{
+		return Collections.max(conn_coo.keySet());
+	}
+}
